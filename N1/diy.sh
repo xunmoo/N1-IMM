7e6a7238ea5db69094840faa27e9482f84369a45
xunmoo <23499618+xunmoo@users.noreply.github.com>
2024-11-19 10:23:43 +0800

    Create diy.sh

---
+++ N1/diy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# Default IP
+sed -i 's/192.168.1.1/192.168.50.3/g' package/base-files/files/bin/config_generate
+
+
+# Add packages
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo package/luci-app-mihomo
+
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/openwrt-passwall
